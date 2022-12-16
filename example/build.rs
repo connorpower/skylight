ae7effc3432b25c87c06e4790c51b0d4ebd50008
Connor Power <connor@connorpower.com>
2022-12-17 12:47:40 +1300

    Add support for named windows resources in example project

--- example/build.rs
+++ example/build.rs
@@ -1,3 +1,6 @@
+// Include the resource definitions we share with the app.
+include!("src/resources.rs");
+
 fn main() {
     if !cfg!(target_os = "windows") {
         panic!("target OS was not Windows")
@@ -5,7 +8,7 @@ fn main() {
 
     // Enable High-DPI support by bundling the `hdpi.manifest`
     ::winres::WindowsResource::new()
-        .set_icon_with_id("icon.ico", "1")
+        .set_icon_with_id(FERRIS_ICON.path(), &FERRIS_ICON.id_string())
         .set_manifest_file("hdpi.manifest")
         .compile()
         .unwrap();
